5c659f5e80e00f3e5897e9e012eb889d969426a2
itwasmegabz <148772602+itwasmegabz@users.noreply.github.com>
2024-07-04 17:57:40 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://nxxbldvpxdkvpwdhybir.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54eGJsZHZweGRrdnB3ZGh5YmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzOTgyMzUsImV4cCI6MjAzNDk3NDIzNX0.XfqZhp-eZFc5JnBF3MBEURf2i2LFLclUJLuiaPqqW2A';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
